7cd8f14c6a4450228331e9f7ca6733e8872db000
Henry Phan <h.phan06@gmail.com>
2019-11-12 18:15:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 require 'pry'
 def key_for_min_value(name_hash)
   lowest_value = nil 
+  lowest_key = nil
   name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value 
       lowest_value = v 
