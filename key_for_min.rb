f98ff37a7429b7573d1ed47682762c6ef1a7eb1a
Henry Phan <h.phan06@gmail.com>
2019-11-12 17:47:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  name_hash.map_with
+  name_hash.each do |key, value|
+    puts "#{key}, #{value}"
 end
